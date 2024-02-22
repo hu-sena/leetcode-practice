a2d2df15b070feca8866420b03e28181591ca7a1
Husna Zubir <102644595+hu-sena@users.noreply.github.com>
2024-02-22 17:29:09 +0800

    Time: 7 ms (34.57%) | Memory: 46.1 MB (50.76%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,25 +1,23 @@
 class Solution {
     public int singleNumber(int[] nums) {
         // occurrences = hashmap
+        // each element appear twice - using XOR to cancel out 2 elements = 0
 
-        Map<Integer, Integer> map = new HashMap<>();
+        // sort array
+        Arrays.sort(nums);
 
-        // insert all nums into hashmap
-        // count occurrences using getOrDefault
-        // REMINDER: get() method in hashmap to get value
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // XOR for [i] + [i + 1] - iterate every 2i: INVALID - out of bound
+        // XOR - no order of operation = cumulative XOR every element
+
+        // initialize XOR as first element to avoid affecting result than 0: INVALID
+        // reason: finding unique elements
+        
+        int XOR = 0;
+        for (int i = 0; i < nums.length; i++) {
+            XOR = XOR ^ nums[i];
         }
 
-        // search among the value which one have only 1 occurrence
-        // using entrySet() method
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1) {
-                return entry.getKey();
-            }
-        }
-
-        return 0;
+        return XOR;
         
     }
 }
